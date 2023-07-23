f550b3208b4764c3251838c35f5b78eb74ac6c64
Doğu Us <uspro@disroot.org>
2023-07-23 17:05:50 +0300

    fix: fix get_memory output for single machine ids

--- hyperv/src/memory.rs
+++ hyperv/src/memory.rs
@@ -10,7 +10,9 @@ pub fn get_memory(machine_id: impl AsRef<str>, pwsh: &mut powershell_rs::Shell) 
             Ok(sout)
         }
     } else {
-        let (sout, serr) = pwsh.execute(format!("Get-VM -Id {machine_id} | Get-VMMemory | Select-Object -Property InstanceID, VirtualQuantity | ConvertTo-Json"))?;
+        // TODO: this is probably a bug, InstanceID and VirtualQuantity properties doesn't exists in the output of Get-VMMemory
+        // let (sout, serr) = pwsh.execute(format!("Get-VM -Id {machine_id} | Get-VMMemory | Select-Object -Property InstanceID, VirtualQuantity | ConvertTo-Json"))?;
+        let (sout, serr) = pwsh.execute(format!(r#"Get-WmiObject -Namespace 'root\virtualization\v2' -Class Msvm_MemorySettingData -Filter "Caption like 'Memory' AND InstanceID like '%{machine_id}%'" | Select-Object -Property InstanceID, VirtualQuantity | ConvertTo-Json"#))?;
         if !serr.is_empty() {
             Err(serr.into())
         } else {
