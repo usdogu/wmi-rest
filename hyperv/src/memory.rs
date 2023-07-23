45a52671bc54a2d2554f7fad670758c3d4e339d5
Doğu Us <uspro@disroot.org>
2023-07-23 17:10:21 +0300

    fix: return err when vm id is not specified in get_memory

--- hyperv/src/memory.rs
+++ hyperv/src/memory.rs
@@ -1,7 +1,7 @@
 pub fn get_memory(machine_id: impl AsRef<str>, pwsh: &mut powershell_rs::Shell) -> Result<String, Box<dyn std::error::Error>> {
     let machine_id = machine_id.as_ref();
     if machine_id.is_empty() {
-        Ok("No VM ID specified".into())
+        Err("No VM ID specified".into())
     } else if machine_id == "all" {
         let (sout, serr) = pwsh.execute(r#"Get-WmiObject -Namespace 'root\virtualization\v2' -Class Msvm_MemorySettingData -Filter "Caption like 'Memory'" | Select-Object -Property InstanceID, VirtualQuantity | ConvertTo-Json"#)?;
         if !serr.is_empty() {
